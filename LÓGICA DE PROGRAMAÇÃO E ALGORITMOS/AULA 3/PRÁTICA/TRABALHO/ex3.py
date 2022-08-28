6ebea04ff7930a879bd73a263e18ba7235e41a6e
Kauã Machado <gnomoscorpions@gmail.com>
2022-08-28 14:11:22 -0300

    algumacoisa

--- LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/TRABALHO/ex3.py
+++ LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/TRABALHO/ex3.py
@@ -1,6 +1,68 @@
-print('Bem-vindo ao programa de feijoada de Kauã Daniel. ')
-print('Menu: ')
-qtd = float(input('Digite a quantidade em ml que deseja: '))
-while qtd < 300 or qtd > 5000:
-    print('Não aceitamos porções menores que 300ml ou 5l. Tente novamente. ')
-    qtd = float(input('Digite a quantidade em ml que deseja: '))
+def volumeFeijoada():
+    while True:
+        try:
+            v = 0
+            qtd = int(input('Digite a quantidade desejada (ml): '))
+            if qtd < 300 or qtd > 5000:
+                qtd = int(input('Digite a quantidade desejada (ml): '))
+            else:
+                v = qtd * 0.08
+                return v
+        except ValueError:
+            print('Digite um valor válido. ')
+
+
+def opcaoFeijoada():
+    print('Entre com a opção desejada:'
+          '\nB - Basica (Feijão + paiol + costelinha)'
+          '\nP - Premium (Feijão + paiol + costelinha + partes de porco)'
+          '\nS - Suprema (Feijão + paiol + costelinha + partes de porco + charque + calabresa + bacon)')
+    escolha = input('').upper()
+    while True:
+        if escolha == 'B':
+            return escolha
+        elif escolha == 'P':
+            return escolha
+        elif escolha == 'S':
+            return escolha
+        else:
+            print('Digite um valor válido. ')
+            print('Entre com a opção desejada:'
+                  '\nB - Basica (Feijão + paiol + costelinha)'
+                  '\nP - Premium (Feijão + paiol + costelinha + partes de porco)'
+                  '\nS - Suprema (Feijão + paiol + costelinha + partes de porco + charque + calabresa + bacon)')
+            escolha = input().upper()
+
+
+def acompanhamentoFeijoada():
+    valorAc = 0
+    print('0- Não desejo mais acompanhamentos (encerrar pedido)'
+          '\n1- 200g de arroz'
+          '\n2- 150g de farofa especial'
+          '\n3- 100g de couve cozida'
+          '\n4- 1 laranja descascada')
+    opc = int(input(''))
+
+    while opc != 0:
+        print('0- Não desejo mais acompanhamentos (encerrar pedido)'
+              '\n1- 200g de arroz'
+              '\n2- 150g de farofa especial'
+              '\n3- 100g de couve cozida'
+              '\n4- 1 laranja descascada')
+        opc = int(input(''))
+        if opc == 1:
+            total = valorAc + 5
+        elif opc == 2:
+            total = valorAc + 6
+        elif opc == 3:
+            total = valorAc + 7
+        elif opc == 4:
+            total = valorAc + 3
+        return total
+
+
+# print('BEM VINDO A FEIJOADA DE KAUÃ MACHADO!!')
+# print('MENU: ')
+# volumeFeijoada()
+# opcaoFeijoada()
+acompanhamentoFeijoada()
--- LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/pythonProject/ex7.py
+++ LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/pythonProject/ex7.py
@@ -31,6 +31,30 @@ if existeArquivos(arquivo):
 else:
     print('Arquivo inexistente. ')
     criaArquivo(arquivo)
+
+
+def cadastrarJogo(nomeArquivo, nomeJogo, nomeVideoGame):
+    try:
+        a = open(nomeArquivo, 'at')
+    except:
+        print('Erro ao abrir o arquivo! ')
+    else:
+        a.write(f'{nomeJogo}; {nomeVideoGame}\n')
+    finally:
+        a.close()
+
+
+def listarArquivo(nomeArquivo):
+    try:
+        a = open(nomeArquivo, 'rt')
+    except:
+        print('Erro ao listar arquivos')
+    else:
+        print(a.read())
+    finally:
+        a.close()
+
+
 while True:
     print('1 - CADASTRAR NOVO ITEM'
           '\n2 - LISTAR TODOS OS ITENS '
@@ -39,9 +63,14 @@ while True:
     esc = valida('Digite a opção desejada:', 1, 3)
 
     if esc == 1:
-        print('Opçãod de cadastrar novo item.')
+        print('CADASTRO: ')
+        nomeJogo = input('Nome do jogo: ')
+        nomeVideoGame = input('Nome do video game: ')
+        cadastrarJogo(arquivo, nomeJogo, nomeVideoGame)
+
     elif esc == 2:
-        print('Opção de listar os itens')
+        print('CADASTRADOS')
+        listarArquivo(arquivo)
     else:
         print('Goodbye')
         break

d92162954f2374a19f3b67a65c70b84cda0e9a17
Kauã Machado <gnomoscorpions@gmail.com>
2022-08-28 17:49:31 -0300

    Exercício 3 resolvido

--- LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/TRABALHO/ex3.py
+++ LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/AULA 3/PRÁTICA/TRABALHO/ex3.py
@@ -1,18 +1,24 @@
 def volumeFeijoada():
+    global v
     while True:
         try:
             v = 0
             qtd = int(input('Digite a quantidade desejada (ml): '))
             if qtd < 300 or qtd > 5000:
+                print('Não aceitamos valores menores que 300 ou maiores que 5000')
                 qtd = int(input('Digite a quantidade desejada (ml): '))
             else:
                 v = qtd * 0.08
-                return v
         except ValueError:
             print('Digite um valor numérico. ')
+            continue
+        return v
+
 
 
 def opcaoFeijoada():
+    global totalOpc
+    v = 1
     print('Entre com a opção desejada:'
           '\nB - Basica (Feijão + paiol + costelinha)'
           '\nP - Premium (Feijão + paiol + costelinha + partes de porco)'
@@ -20,11 +26,11 @@ def opcaoFeijoada():
     escolha = input('').upper()
     while True:
         if escolha == 'B':
-            return escolha
+            totalOpc = v * 1
         elif escolha == 'P':
-            return escolha
+            totalOpc = v * 1.25
         elif escolha == 'S':
-            return escolha
+            totalOpc = v * 1.5
         else:
             print('Digite um valor válido. ')
             print('Entre com a opção desejada:'
@@ -32,33 +38,48 @@ def opcaoFeijoada():
                   '\nP - Premium (Feijão + paiol + costelinha + partes de porco)'
                   '\nS - Suprema (Feijão + paiol + costelinha + partes de porco + charque + calabresa + bacon)')
             escolha = input().upper()
+            continue
+        return totalOpc
 
 
-def acompanhamentoFeijoada(total=0):
+def acompanhamentoFeijoada():
+    v1 = v2 = v3 = v4 = 0
     valorAc = 0
+    global totalAdc
+    while True:
+        print('0- Não desejo mais acompanhamentos (encerrar pedido)'
+              '\n1- 200g de arroz'
+              '\n2- 150g de farofa especial'
+              '\n3- 100g de couve cozida'
+              '\n4- 1 laranja descascada')
+        opc = int(input(''))
+        if opc == 1:
+            v1 = valorAc + 5
+            print('200g de arroz foram adicionadas! ')
+            continue
+        elif opc == 2:
+            v2 = valorAc + 6
+            print('150g de farofa especial foram adicionadas! ')
+            continue
+        elif opc == 3:
+            v3 = valorAc + 7
+            print('100g de couve foram adicionadas! ')
+            continue
+        elif opc == 4:
+            v4 = valorAc + 4
+            print('1 (uma) laranja foi adicionada! ')
+            continue
+        if opc < 0 or opc > 4:
+            print('Digite uma opção entre 0 e 4')
+            continue
+        totalAdc = v1 + v2 + v3 + v4
+        return totalAdc
 
-    print('0- Não desejo mais acompanhamentos (encerrar pedido)'
-          '\n1- 200g de arroz'
-          '\n2- 150g de farofa especial'
-          '\n3- 100g de couve cozida'
-          '\n4- 1 laranja descascada')
-    opc = int(input(''))
-    if opc == 0:
-        print('Obrigado')
-    elif opc == 1:
-        total = valorAc + 5
-        return total
-    elif opc == 2:
-        total = valorAc + 6
-    elif opc == 3:
-        total = valorAc + 7
-    elif opc == 4:
-        total = valorAc + 4
-        print('150g de farofa foram adicionados! ')
-        return total
 
-# print('BEM VINDO A FEIJOADA DE KAUÃ MACHADO!!')
+print('BEM VINDO A FEIJOADA DE KAUÃ MACHADO!!')
 print('MENU: ')
-# volumeFeijoada()
-# opcaoFeijoada()
+volumeFeijoada()
+opcaoFeijoada()
 acompanhamentoFeijoada()
+soma = (v * totalOpc) + totalAdc
+print(f'O total a pagar é {soma}. Volume = {v} * opção = {totalOpc} + acompanhamento = {totalAdc} ')
